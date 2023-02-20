9f6a8bd7195920defa032ea10e8ae82e4d188520
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-19 20:57:22 -0800

    lowercase uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char lowercase, uppercase;
+
+	/* Print lowercase letters */
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+		letter++;
+	}
+
+	/* Print uppercase letters */
+	for (uppercase = 'A'; uppercase <= 'Z'; ippercase++)
+	{
+		putchar(uppercase)
+	}
+
+	/* Print new line */
+	putchar('\n')
+
+	return (0);
+}
