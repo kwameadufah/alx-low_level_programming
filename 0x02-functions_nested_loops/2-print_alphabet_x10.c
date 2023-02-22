b84e64949d0d37c5699a1d1831f033dc8f4734ef
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-22 15:34:42 -0800

    update 0-1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,20 @@
 #include "main.h"
 /**
- * Print alphabet in lowercase, followed by a new line 
+ * Print alphabet in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {
 	char letter;
+	int times = 0;
 
-	for (letter = 'a'; letter <= 'z'; letter++);
-		_putchar(letter);
+	while (times++ <= 9)
 
-	_putchar('\n');
+	{
+		for (letter = 'a'; letter <= 'z'; letter++);
+			_putchar(letter);
+
+		_putchar('\n');
+	}
 
 	return (0);
 }
