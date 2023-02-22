64fd4bd2bf8936865f7f3c145620da376b5ef293
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-22 14:14:48 -0800

    Print _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: Prince Adufah
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+int _putchar(char a):
+void print_alphabet(void);
+void print_alphabet_x10(void);
