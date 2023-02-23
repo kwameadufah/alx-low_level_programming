34883e36829791fb4856785a86b779d09f7c9bac
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-22 17:47:56 -0800

    updated codes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * Print alphabet in lowercase, followed by a new line 
+ * print_alphabet - Prints alphabet in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++);
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * Print alphabet in lowercase, followed by a new line
+ * print_alphabet_x10 -  Prints alphabet in lowercase ten times
  */
 void print_alphabet(void)
 {
@@ -10,7 +10,7 @@ void print_alphabet(void)
 	while (times++ <= 9)
 
 	{
-		for (letter = 'a'; letter <= 'z'; letter++);
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 
 		_putchar('\n');
