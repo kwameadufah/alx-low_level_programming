c41b25d876e56032f0439f500f82d45c756eaa3f
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-22 18:45:25 -0800

    Adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers 
+ * @num1: First integer
+ * @num2: Second integer 
+ *
+ * Return: Result of the addition
+ */
+int add(int num1 , int num2)
+{
+	return(num1 + num2);
+
+}
