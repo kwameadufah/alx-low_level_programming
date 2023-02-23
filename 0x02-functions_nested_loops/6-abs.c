4960332a6244be927a3975879e5c5fa365c1ae6d
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-22 18:20:24 -0800

    Compute absolute value of an interger

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _abs - computes the absolute value of an integer
+ * @n: The interger to be computed.
  *
  * Return: Always 0.
  *
  */
-int _abs(int);
+int _abs(int n)
 {
 	if (n >= 0)
 		return (n);
