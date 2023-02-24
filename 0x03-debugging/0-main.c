d255dbf30cf1b55d702bef2328a75b01dce8c681
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-24 13:12:20 -0800

    test positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,21 +5,12 @@
 * Return: 0
 */
 
-void positive_or_negative(void)
+int main(void)
 {
-	int i;
+        int i;
 
-	i = 98;
-	positive_or_negative(i);
-	assert(strcmp(get_stdout(), "98 is positve\n") == 0);
+        i = 98;
+        positive_or_negative(i);
 
-	i = -98;
-	positive_or_negative(i);
-	assert(strcmp(get_stdout(), "-98 is negative\n") == 0);
-
-	i = 0;
-	positive_or_negative(i);
-	assert(strcmp(get_stdout(), "0 is zero\n") == 0);
-
-	return (0);
+        return (0);
 }
