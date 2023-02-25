388296f48c5137a245826e6309a4fc2ca0d40439
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-24 16:20:44 -0800

    Draw a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- *
- * print_line - Draws a straight line using the character _.
+ * print_line - Draw a straight line using the character _.
  * @n: The number of _ characters to be printed.
- *
  */
 void print_line(int n)
 {
