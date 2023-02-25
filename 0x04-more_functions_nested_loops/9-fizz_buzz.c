8ba4676122945edff004c7edfd777818989e265a
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-24 17:31:51 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - Print numbers from 1 to 100, for multiples of three,
+ *	Prints Fizz, for multiples of five print Buzz,
+ *	and FizzBuzz for multiples of both
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 3) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
