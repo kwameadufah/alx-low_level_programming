756766ec8211de1dc8de9e43a5e364869dab9950
Prince Adufah <prince.adufah@sciencespo.fr>
2023-02-28 00:22:38 -0800

    Print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@ void print_rev(char *s)
 {
 	int length = 0;
 	int i;
-	
+
 	while (*(s + length) != '\0')
 		length++;
 
 	for (i = length - 1; i >= 0; i--)
 		_putchar(*(s + i));
-	
+
 	_putchar('\n');
 }
