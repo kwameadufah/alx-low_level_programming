5df062da69c2dae9003a933736e022121036fcec
Prince Adufah <prince.adufah@sciencespo.fr>
2023-06-07 10:52:55 -0700

    completed tasks

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings, considering wildcard characters.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared - may contain wildcards.
+ *
+ * Return: If the strings can be considered identical - 1.
+ *         Otherwise - 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return 1;
+	}
+
+	if (*s1 == *s2)
+	{
+		return wildcmp(s1 + 1, s2 + 1);
+	}
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) != '*' && *s1 != '\0')
+		{
+			return wildcmp(s1 +1, s2) || wildcmp (s1, s2 + 1);
+		}
+
+		return wildcmp(s1, s2 + 1);
+	}
+
+	return 0;
+}
